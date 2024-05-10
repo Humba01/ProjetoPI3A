6cdf69fe43be80b21c33e54caf87fa50e8b993f2
Humba01 <humbandroid@gmail.com>
2024-05-10 12:11:24 -0300

    Adicionando condicionais

--- app/src/main/java/papinho/app/userinterface/InterfaceUsuario.java
+++ app/src/main/java/papinho/app/userinterface/InterfaceUsuario.java
@@ -4,5 +4,13 @@ public class InterfaceUsuario extends InterfaceComponentes {
     public InterfaceUsuario() {}
 
     @Override
-    public void trocandoTema() {}
+    public void trocandoTema() {
+        if(light_checkbox.isChecked()) {}
+        if(dark_checkbox.isChecked()) {}
+        if(light_protanopia_and_deuteranopia_checkbox.isChecked()) {}
+        if(dark_protanopia_and_deuteranopia_checkbox.isChecked()) {}
+        if(light_tritanopia_checkbox.isChecked()) {}
+        if(dark_tritanopia_checkbox.isChecked()) {}
+    }
+
 }
